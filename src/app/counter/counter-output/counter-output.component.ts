1c2a66c1246a3d86c5f2610d3102d7f761b40c9c
adamswonder <adamswill.i.am71002@gmail.com>
2023-04-29 23:48:11 +0300

    ⏫Feat:inject store counter to the output

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
@@ -6,8 +7,12 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./counter-output.component.css'],
 })
 export class CounterOutputComponent implements OnInit {
-  @Input() counter: any;
+  counter!: number;
+  constructor(private store: Store<{ counter: { counter: number } }>) {}
 
-  constructor() {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.store.select('counter').subscribe(data =>{
+      this.counter = data.counter
+    } )
+  }
 }
